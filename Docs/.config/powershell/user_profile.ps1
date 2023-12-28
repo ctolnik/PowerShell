44d0f843158ca8dc6681d16656a4c525c8682308
ctolnik <62750781+ctolnik@users.noreply.github.com>
2023-12-28 15:25:55 +0300

    Update user_profile.ps1

--- Docs/.config/powershell/user_profile.ps1
+++ Docs/.config/powershell/user_profile.ps1
@@ -1,7 +1,7 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+# Load Prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) ".\ilyak.omp.json"
+oh-my-posh -init -shell pwsh -config $PROMPT_CONFIG | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
@@ -22,13 +22,4 @@ $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
-Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
+Set-Alias grep findstr                                                                                                                                      Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'                                                                                                        Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'                                                                                                                                                                                                                                                                  # Utilities                                                                                                                                                 function which ($command) {                                                                                                                                   Get-Command -Name $command -ErrorAction SilentlyContinue |                                                                                                    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue                                                                                        }
